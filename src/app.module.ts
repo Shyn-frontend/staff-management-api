e0ddd2413b6283507722458775f37ada35a7a7ae
Hong Quang <danghongquang99@gmail.com>
2021-01-02 16:12:34 +0700

    config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from './shared/config/config.module';
 import { UserController } from './user/user.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+  ],
   controllers: [UserController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -5,36 +5,45 @@ import * as helmet from 'helmet';
 
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { IConfig } from './config.interface';
+import { CONFIG } from './shared/config/config.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.get<IConfig>(CONFIG);
+  const logger = new Logger('Baseline-clone');
+
+  logger.debug(`Environment: ${config.app.env}`);
 
   app.use(compression());
   app.use(cors());
   app.use(helmet());
 
-  const options = new DocumentBuilder()
-    .setTitle('Baseline clone')
-    .setDescription('Baseline Nestjs clone APIs description')
-    .setVersion('1.0')
-    .addTag('baseline-nestjs')
-    .build();
+  if (config.app.isSwaggerEnabled) {
+    const options = new DocumentBuilder()
+      .setTitle('Baseline clone')
+      .setDescription('Baseline Nestjs clone APIs description')
+      .setVersion('1.0')
+      .addTag('baseline-nestjs')
+      .build();
 
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document, {
-    swaggerOptions: {
-      docExpansion: 'none',
-      explorer: true,
-      filter: true,
-      showRequestDuration: true,
-    },
-  });
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document, {
+      swaggerOptions: {
+        docExpansion: 'none',
+        explorer: true,
+        filter: true,
+        showRequestDuration: true,
+      },
+    });
+    logger.debug(`Swagger Docs enabled: ${config.app.domain}/docs`);
+  }
 
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
-      transform: true,
+      transform: true,  
       whitelist: true,
       forbidNonWhitelisted: true,
     })
