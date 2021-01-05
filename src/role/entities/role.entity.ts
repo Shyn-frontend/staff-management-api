d68ca1651aaf134ac922b1d412e0cd3593793c8f
Hong Quang <danghongquang99@gmail.com>
2021-01-05 21:12:42 +0700

    init role controller, module, service, entity

--- src/role/entities/role.entity.ts
+++ src/role/entities/role.entity.ts
@@ -1,3 +1,27 @@
-export class Role {
-  
+import { BaseEntity, Column, Entity, Index, PrimaryGeneratedColumn } from "typeorm";
+
+export enum ROLES {
+  ADMIN_ROLE = 'Admin',
+  HR_ADMIN_ROLE = 'HR Admin',
+  TEAM_HEAD_ROLE = 'Team Head',
+  PROJECT_ADMIN_ROLE = 'Project Admin',
+  PROJECT_MANAGER_ROLE = 'Project Manager',
+  EMPLOYEE_ROLE = 'Employee',
+  CLIENT_ROLE = 'Client',
+}
+@Entity('role')
+@Index(['id'])
+export class Role extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column({
+    type: 'enum',
+    enum: ROLES,
+    default: ROLES.EMPLOYEE_ROLE
+  })
+  name: ROLES;
+
+  @Column()
+  order: number;
 }
