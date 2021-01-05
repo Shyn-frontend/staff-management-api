14f73c947865475b680316205a67b188ab286328
Hong Quang <danghongquang99@gmail.com>
2021-01-05 21:13:30 +0700

    init position controller, module, service, entity

--- src/position/entities/position.entites.ts
+++ src/position/entities/position.entites.ts
@@ -1,8 +1,13 @@
 import { IsString, Length } from "class-validator";
 import { Department } from "src/department/entities/department.entity";
-import { BaseEntity, Column, JoinColumn, OneToOne } from "typeorm";
+import { BaseEntity, Column, Entity, Generated, Index, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 
+@Entity('position')
+@Index(['id'])
 export class Position extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: string;
+
   @Column()
   @IsString()
   @Length(1, 128)
