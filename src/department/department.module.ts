fadcf49be6b63e758badc1caab502721343223bd
Hong Quang <danghongquang99@gmail.com>
2021-02-04 23:40:50 +0700

    remove controller in module

--- src/department/department.module.ts
+++ src/department/department.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Department } from '../entities/department.entity';
 import { DepartmentService } from './department.service';
-import { DepartmentController } from './department.controller';
 import { UserModule } from 'src/user/user.module';
 import DepartmentResolver from './resolvers/department.resolver';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Department]), UserModule],
   providers: [DepartmentService, DepartmentResolver],
-  controllers: [DepartmentController],
   exports: [DepartmentService],
 })
 export class DepartmentModule {}
--- src/position/position.module.ts
+++ src/position/position.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DepartmentModule } from 'src/department/department.module';
 import { Position } from '../entities/position.entity';
-import { PositionController } from './position.controller';
 import { PositionService } from './position.service';
 import PositionResolver from './resolvers/position.resolver';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Position]), DepartmentModule],
   providers: [PositionService, PositionResolver],
-  controllers: [PositionController],
 })
 export class PositionModule {}
