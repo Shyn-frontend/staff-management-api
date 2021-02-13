4e21bed528c41f691e84ca685441f1c813c3bed0
Hong Quang <danghongquang99@gmail.com>
2021-02-13 09:24:58 +0700

    create map user to managerDto

--- src/user/profile/user.profile.ts
+++ src/user/profile/user.profile.ts
@@ -1,7 +1,9 @@
 import { MappingProfile } from '@automapper/types';
 import { AuthUserDto } from 'src/auth/dto/auth-user.dto';
 import { User } from 'src/entities/user.entity';
+import { ManagerDto } from '../dto/manager.dto';
 
 export const UserProfile: MappingProfile = (mapper) => {
   mapper.createMap(User, AuthUserDto);
+  mapper.createMap(User, ManagerDto);
 };
