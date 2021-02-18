8f623db408c8de8534de3ceb917c276b72bddaae
Hong Quang <danghongquang99@gmail.com>
2021-02-19 00:33:47 +0700

    add field metas in user entity

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,9 +1,17 @@
 import { Position } from './position.entity';
 import { Role } from './role.entity';
 import { Base } from '../shared/base.entity';
-import { Column, Entity, Index, JoinColumn, ManyToOne, Unique } from 'typeorm';
+import {
+  Column,
+  Entity,
+  Index,
+  JoinColumn,
+  ManyToOne,
+  Unique,
+} from 'typeorm';
 import { USER_TYPE } from '../user/enum/user-type.enum';
 import { AutoMap } from '@automapper/classes';
+import { UserMeta } from './user-meta.entity';
 
 @Entity()
 @Unique(['email'])
@@ -28,6 +36,9 @@ export class User extends Base {
   })
   @AutoMap()
   name?: string;
+
+  @AutoMap(() => UserMeta)
+  metas: UserMeta[];
 
   @Column({
     type: 'varchar',
