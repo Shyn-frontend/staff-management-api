5f40c942d3c22dfdc3cd0b5847062b43609ecb80
Hong Quang <danghongquang99@gmail.com>
2021-02-18 14:27:49 +0700

    update README & command run migrations

--- src/migration_db.sh
+++ src/migration_db.sh
@@ -1,4 +1,4 @@
 ts-node ./node_modules/typeorm/cli.js schema:drop
-ts-node ./node_modules/typeorm/cli.js migration:run
+ts-node ./node_modules/typeorm/cli.js schema:sync
 ts-node ./node_modules/typeorm-seeding/dist/cli.js seed
 # typeorm seed:run --seed 20191031084601-init-admin-user.js
